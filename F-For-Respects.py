c906825cf82b4c2daf813e480c5c7b8bb03edc1b
lewiqs <35903191+lewiqs@users.noreply.github.com>
2018-01-29 17:58:03 +0000

    F for Respects Bot

---
+++ F-For-Respects.py
@@ -0,0 +1,23 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print ("Ready to use")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    elif message.content.startswith("f"):
+        e = discord.Embed()
+        e.set_image(url=('/images/respect.jpg'))
+        await client.send_message(message.channel, embed=e)
+    elif message.content.startswith("F"):
+        e = discord.Embed()
+        e.set_image(url=('/images/respect.jpg'))
+        await client.send_message(message.channel, embed=e)
+        
+client.run("YOURBOTTOKEN")
